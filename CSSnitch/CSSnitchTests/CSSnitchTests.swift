dceaee834c6d0a7a25f5c157da3fd65f02df3a55
casper <c.groenenberg@hotmail.com>
2025-07-24 16:13:06 +0200

    start setup

---
+++ CSSnitch/CSSnitchTests/CSSnitchTests.swift
@@ -0,0 +1,17 @@
+//
+//  CSSnitchTests.swift
+//  CSSnitchTests
+//
+//  Created by Casper Groenenberg on 24/07/2025.
+//
+
+import Testing
+@testable import CSSnitch
+
+struct CSSnitchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
